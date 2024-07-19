3c9cd8af021417ff173c687b97be9755a0dde836
Claudiu Belu <cbelu@cloudbasesolutions.com>
2024-07-19 15:04:06 +0000

    sanity: Uses k8s_test_harness utils
    The k8s_test_harness contains commonly used util functions. This
    will simplify the tests.

--- tests/integration/test_fluent_bit.py
+++ tests/integration/test_fluent_bit.py
@@ -2,12 +2,9 @@
 # Copyright 2024 Canonical, Ltd.
 #
 import logging
-import os
 
 from k8s_test_harness import harness
-from k8s_test_harness.util import exec_util
 from k8s_test_harness.util import env_util, k8s_util
-
 
 pytest_plugins = ["k8s_test_harness.plugin"]
 
@@ -19,33 +16,17 @@ def test_integration_fluent_bit(module_instance: harness.Instance):
         "fluent-bit", "2.1.6", "amd64"
     )
 
-    images = [
-        k8s_util.HelmImage(fluent_bit_rock.image)
-    ]
+    images = [k8s_util.HelmImage(fluent_bit_rock.image)]
 
     helm_command = k8s_util.get_helm_install_command(
         "fluent-bit",
         "fluent-bit",
         namespace="fluent-bit",
         repository="https://fluent.github.io/helm-charts",
-        chart_version="0.34.2", # chart version with 2.1.6 app
+        chart_version="0.34.2",  # chart version with 2.1.6 app
         images=images,
     )
 
     module_instance.exec(helm_command)
 
-    exec_util.stubbornly(retries=5, delay_s=5).on(module_instance).exec(
-        [
-            "k8s",
-            "kubectl",
-            "wait",
-            "--for=condition=ready",
-            "pod",
-            "-l",
-            "app.kubernetes.io/name=fluent-bit",
-            "--namespace",
-            "fluent-bit",
-            "--timeout",
-            "180s",
-        ]
-    )
+    k8s_util.wait_for_daemonset(module_instance, "fluent-bit", "fluent-bit")
